d26f427bd7f903f3b3cfbee689dc733366a3826a
quyen <quyendtgbh17114@fpt.edu.vn>
2019-07-29 10:53:29 +0700

    fff

--- Product.php
+++ Product.php
@@ -1,8 +1,8 @@
 <?php
 include 'db.php';
     // Check connection
-    $sql = "SELECT ProductId, Image, Price, ProductName, Origin FROM product";
-                    $result = pg_query($connection,$sql);
+    $sql = "SELECT * FROM product";
+         $result = pg_query($connection,$sql);
     if (pg_num_rows($result) > 0) {
         // output data of each row
         while($row = pg_fetch_assoc($result)) {
@@ -12,13 +12,13 @@ include 'db.php';
             $Origin = $row['Origin']
             $ProductName = $row['ProductName'];
             ?>
-        <div class="bodyRightContainer" action="Product.php">
+         <div class="bodyRightContainer" action="Product.php">
             <a href="./Product-detail.php?ProductId=<?php echo $row["ProductId"]?>">
                 <img src= "<?php echo $row["Image"]?>" style="width: 200px; height: 250px;">
             </a><br>
             <span><?php echo $row["ProductName"]?></span><br>
             <span>Origin:<?php echo $row["Origin"]?></span><br>
-            <span><a href="#"><img src="images/crafs.png"></a> <?php echo $row["Price"]?>$</span>
+            <span><a href="Buy.html"><img src="images/crafs.png"></a> <?php echo $row["Price"]?>$</span>
         </div>
     <?php
         }
--- db.php
+++ db.php
@@ -1,5 +1,7 @@
 <?php
-$connection = pg_connect("host=ec2-174-129-41-127.compute-1.amazonaws.com port=5432 dbname=dbuicb2fad8nel user=swihshhekuhogs password=16d7781adcc13a2503c3a23d4cea48d62764c13a71ecb9b67fe40978a72d55d2");  
+$connection = pg_connect("host=ec2-174-129-41-127.compute-1.amazonaws.com port=5432 
+    dbname=dbuicb2fad8nel user=swihshhekuhogs 
+    password=16d7781adcc13a2503c3a23d4cea48d62764c13a71ecb9b67fe40978a72d55d2");  
  if(!$connection) {
      die("Database connection failed");
  }
