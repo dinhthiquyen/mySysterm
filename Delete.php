17c5ab4f397d93a61e03eb2458a542627a8076df
quyen <quyendtgbh17114@fpt.edu.vn>
2019-08-26 11:03:35 +0700

    Update Delete.php

--- Delete.php
+++ Delete.php
@@ -1,8 +1,8 @@
 <?php  
-require_once 'db.php';
+include 'db.php';
 $ProductId = $_GET['ProductId'];
 
-$sql2 ="DELETE FROM product WHERE ProductId = '$ProductId'";
+$sql2 ="DELETE FROM product WHERE ProductId = '".$ProductId."'";
 
 $result = pg_query($sql2);
 $cmdtuples = pg_affected_rows($result);
