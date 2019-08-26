15ba8a370a32bfadd2a1cc8ed319d2bc8682e850
quyen <quyendtgbh17114@fpt.edu.vn>
2019-08-26 11:35:06 +0700

    1

--- Delete.php
+++ Delete.php
@@ -4,7 +4,14 @@ $ProductId = $_GET['ProductId'];
 
 $sql2 ="DELETE FROM product WHERE ProductId = '$ProductId'";
 
-pg_query($connection, $sql2);
+$result = pg_query($connection, $sql2);
+$cmdtuples = pg_affected_rows($result);
+echo $cmdtuples . " record affected.\n";
+if (!$result) {
+    $errormessage = pg_last_error();
+    echo "Error with query: " . $errormessage;
+    exit();
+}
 
 header('location:https://selling-toys.herokuapp.com/Admin.php');
 
