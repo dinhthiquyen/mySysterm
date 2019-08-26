7422ad1109110a4e583f52552d1227325a61869f
quyen <quyendtgbh17114@fpt.edu.vn>
2019-08-26 12:09:52 +0700

    hi

--- Delete.php
+++ Delete.php
@@ -2,13 +2,18 @@
 include 'db.php';
 $ProductId = $_GET['ProductId'];
 
-$sql2 ="DELETE * FROM product WHERE product.ProductId = '$ProductId'";
+$sql2 ="DELETE FROM product WHERE ProductId = '$ProductId'";
 
-//$result = pg_query($connection,$sql2);
-$stmt = $connection->prepare($sql2);
-$stmt->execute();
-//$cmdtuples = pg_affected_rows($result);
-//echo $cmdtuples . " record affected.\n";
+$result = pg_query($connection,$sql2);
+$cmdtuples = pg_affected_rows($result);
+echo $cmdtuples . " record affected.\n";
+if (!$result) {
+    $errormessage = pg_last_error();
+    echo "Error with query: " . $errormessage;
+    exit();
+}
+pg_close();
 
+header('location:https://selling-toys.herokuapp.com/Admin.php');
 
 ?>
