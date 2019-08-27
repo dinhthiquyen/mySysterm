0d1b6e5d7d08ee666a00af5bb2db4d3dd856710a
quyen <quyendtgbh17114@fpt.edu.vn>
2019-08-27 16:02:18 +0700

    Update Update.php

--- Update.php
+++ Update.php
@@ -1,8 +1,9 @@
 <?php
 include_once 'db.php';
 if(count($_POST)>0) {
-pg_query($conn,"UPDATE product set productid='" . $_POST['id'] . "', productname='" . $_POST['PrName'] . "', origin ='" . $_POST['origin'] . "', price ='" . $_POST['Price'] . "', description='" . $_POST['Des'] . "' ,email='" . $_POST['email'] . "' WHERE productid='" . $_POST['id'] . "'");
+pg_query($connection,"UPDATE product set productid='" . $_POST['id'] . "', productname='" . $_POST['PrName'] . "', origin ='" . $_POST['origin'] . "', price ='" . $_POST['Price'] . "', description='" . $_POST['Des'] . "' ,email='" . $_POST['email'] . "' WHERE productid='" . $_POST['id'] . "'");
 $message = "Record Modified Successfully";
+header("Location: ./Admin.php");
 }
 $result = pg_query($connection,"SELECT * FROM product WHERE productid='" . $_GET['id'] . "'");
 $row= pg_fetch_array($result);
