365d1b10c60f6972b33b2413dc13aa4aaacbe413
quyen <quyendtgbh17114@fpt.edu.vn>
2019-09-01 14:37:54 +0700

    g

--- AddProduct.php
+++ AddProduct.php
@@ -7,13 +7,13 @@
     $Des="";
     $Price="";
 
-    if(isset($_GET['Id'])&& isset($_GET['PrName'])&& isset($_GET['Origin'])&& isset($_GET['Des'])&& isset($_GET['Price']))
+    if(isset($_POST['Id'])&& isset($_POST['PrName'])&& isset($_POST['Origin'])&& isset($_POST['Des'])&& isset($_POST['Price']))
     {   
-        $Id = $_GET['Id'];
-        $PrName = $_GET['PrName'];
-        $Origin = $_GET['Origin'];
-        $Des = $_GET['Des'];
-        $Price = $_GET['Price'];
+        $Id = $_POST['Id'];
+        $PrName = $_POST['PrName'];
+        $Origin = $_POST['Origin'];
+        $Des = $_POST['Des'];
+        $Price = $_POST['Price'];
         //tạo kết nối tới csdl
         //lấy tất cả các bản ghi từ kết quả
         $sql = "INSERT INTO  product(productid, productname, origin, description, price)VALUES ('$Id', '$PrName', '$Origin', '$Des', '$Price')";  
@@ -22,4 +22,4 @@
         header("Location: ./Admin.php"); /* Redirect browser */
         
     }
-    ?>
+?>
--- SignUp.php
+++ SignUp.php
@@ -1,33 +1,22 @@
 
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Set data</title>
-	
-</head>
-<body>
-<?php
-    include 'db.php';
 
+<?php 
+
+    include 'db.php';
     $uname = "";
     $psw = "";
 
-    //Lấy giá trị POST từ form vừa submit
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        if(isset($_POST["uname"])) { $uname = $_POST['uname']; }
-        if(isset($_POST["psw"])) { $psw = $_POST['psw']; }
-    $sql = "INSERT INTO accountuser(username, password)
-    VALUES ('$uname', '$psw')";
+    if(isset($_POST['uname'])&& isset($_POST['psw']))
+    {   
+        $psw = $_POST['psw'];
+        $uname = $_POST['uname'];
+        
+        //tạo kết nối tới csdl
+        //lấy tất cả các bản ghi từ kết quả
+        $sql = "INSERT INTO accountuser(username, password) VALUES ('$uname', '$psw')";
 
-    if (pg_query($connection, $sql)) {
-            echo "Sign up succesfully"; 
-            header("Location: ./index.php"); /* Redirect browser */
-        } else {
-            echo "Sign up fail ";
-        }
+        pg_query($connection ,$sql);
+        header("Location: ./index.php"); /* Redirect browser */
+        
     }
-
 ?>
-
-</body>
-</html>
